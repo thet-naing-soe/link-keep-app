352cd65901735787e4de24ebe1923d06caa8fbda
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-07-06 13:21:22 +0630

    change next-auth@5 to next-auth@4

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHub from 'next-auth/providers/github';
 import { prisma } from '@/lib/prisma';
 
